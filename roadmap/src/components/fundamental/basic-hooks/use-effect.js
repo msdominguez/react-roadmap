b73938539299786f4cfb90732ea9518d09162c11
msdominguez <m2015dominguez@gmail.com>
2021-08-23 19:06:49 -0400

    updated use effect

--- roadmap/src/components/fundamental/basic-hooks/use-effect.js
+++ roadmap/src/components/fundamental/basic-hooks/use-effect.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function ExampleUseEffect() {
   const [city, setCity] = useState("West Chester");
+  const [temp, setTemp] = useState("0");
 
   useEffect(() => {
     fetch(
@@ -10,21 +11,18 @@ function ExampleUseEffect() {
       .then((res) => res.json())
       .then(
         (result) => {
-          document.getElementById(
-            "city"
-          ).innerHTML = `temperature in degrees F: ${result.current.temp_f}`;
+          setTemp(result.current.temp_f);
         },
         (error) => {
-          document.getElementById(
-            "city"
-          ).innerHTML = `error during API call: ${error}`;
+          setTemp("0");
         }
       );
-  }, [city]); // only re-run the effect if name changes (componentwillUnmount, componentdidMount)
+  }, [city, temp]); // only re-run the effect if name changes (componentwillUnmount, componentdidMount)
 
   return (
     <div>
       <p>city: {city}</p>
+      <p>temperature in degrees F: {temp}</p>
       <button onClick={() => setCity("Philadelphia")}>Get new city</button>
     </div>
   );
@@ -38,7 +36,6 @@ class UseEffect extends React.Component {
       <div>
         <h1>useEffect:</h1>
         <ExampleUseEffect />
-        <h2 id="city"></h2>
       </div>
     );
   }
